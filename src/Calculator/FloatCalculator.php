81692c87cf424dbc22050296f92eefaf65b91c95
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-08 11:08:03 +0100

    ajout de la classe FloatCalculator

---
+++ src/Calculator/FloatCalculator.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Calculator;
+
+class FloatCalculator
+{
+    /**
+     * Méthode d'addition de deux nombres flottants
+     * @param float $firstOperand Premier opérande
+     * @param float $secondOperand Second opérande
+     * @return float Somme des deux opérandes
+     */
+    public function add(float $firstOperand, float $secondOperand): float
+    {
+        return $firstOperand+$secondOperand;
+    }
+    public function subtract(float $firstOperand, float $secondOperand): float
+    {
+        return $firstOperand-$secondOperand;
+    }
+    public function multiply(float $firstOperand, float $secondOperand): float
+    {
+        return $firstOperand*$secondOperand;
+    }
+    public function divide(float $firstOperand, float $secondOperand): float
+    {
+        if ($secondOperand == 0) {
+            throw new \RuntimeException();
+        }
+        return $firstOperand/$secondOperand;
+    }
+    public function modulus(float $firstOperand, float $secondOperand): float
+    {
+        if ($secondOperand == 0) {
+            throw new \RuntimeException();
+        }
+        return $firstOperand%$secondOperand;
+    }
+    public function sum(...$floats): float
+    {
+        $result = 0;
+        foreach ($floats as $float) {
+            $result += $float;
+        }
+        return $result;
+    }
+}
