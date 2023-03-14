9c9f72f5b99504908224069f7c2cfc248a13adf4
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-14 12:10:16 +0100

    ajout du test pour vérifier la précision

--- tests/Calculator/FloatCalculatorTest.php
+++ tests/Calculator/FloatCalculatorTest.php
@@ -26,6 +26,23 @@ class FloatCalculatorTest extends TestCase
             [-1.8, -0.2, -2]
         ];
     }
+    public function sumProvider(): array
+    {
+        return [
+            [6.0, ...[1,2,3]],
+            [0.0, ...[0]],
+            [-2.0, ...[-1,-1]],
+            [100.0, ...[10,20,30,40]]
+        ];
+    }
+    public function modulusProvider(): array
+    {
+        return [
+            '1. % 1 = 0' => [1,1,0],
+            '10. % 3 = 1' => [10,3,1],
+            '5. % 2.4 = 0.2' => [5,2.4,0.2],
+        ];
+    }
     /**
      * @return void
      */
@@ -62,21 +79,18 @@ class FloatCalculatorTest extends TestCase
         $this->expectException(RuntimeException::class);
         $this->floatCalculator->divide(1, 0);
     }
-    public function testModulus(): void
+
+    /**
+     * @param float $firstOperand
+     * @param float $secondOperand
+     * @dataProvider  modulusProvider
+     * @return void
+     */
+    public function testModulus(float $firstOperand, float $secondOperand, float $expected): void
     {
-        assertSame(1.0, $this->floatCalculator->divide(1, 1));
-        assertSame(5.0, $this->floatCalculator->divide(10, 2));
+        assertSame($expected, $this->floatCalculator->modulus($firstOperand, $secondOperand));
         $this->expectException(RuntimeException::class);
         $this->floatCalculator->divide(1, 0);
-    }
-    public function sumProvider(): array
-    {
-        return [
-            [6.0, ...[1,2,3]],
-            [0.0, ...[0]],
-            [-2.0, ...[-1,-1]],
-            [100.0, ...[10,20,30,40]]
-        ];
     }
 
     /**
