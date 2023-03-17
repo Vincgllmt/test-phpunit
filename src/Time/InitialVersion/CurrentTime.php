b1ed7d3cbc0cc98e5bbc89ccf2d13afad9cf2669
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-17 10:40:41 +0100

    ajout méthode getTimeOfDay

--- src/Time/InitialVersion/CurrentTime.php
+++ src/Time/InitialVersion/CurrentTime.php
@@ -10,4 +10,18 @@ class CurrentTime
     {
         return (int)date('G');
     }
+    public function getTimeOfDay(): string
+    {
+        $date = $this->getTime();
+        switch ($date) {
+            case ($date < 6):
+                return "Night";
+            case ($date > 6 && $date < 12):
+                return "Morning";
+            case ($date > 12 && $date < 18):
+                return "Noon";
+            default:
+                return "Evening";
+        }
+    }
 }
